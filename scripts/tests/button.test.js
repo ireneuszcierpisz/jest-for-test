54b1951553cf988e4bcfc646469efd5df670c79c
ireneuszcierpisz <>
2023-12-10 19:57:34 +0000

    Mocking. Test for changes in html.

---
+++ scripts/tests/button.test.js
@@ -0,0 +1,18 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const buttonClick = require("../button");
+
+beforeAll(() => {
+    document.body.innerHTML = "<p id='par'></p>";
+});
+
+// create test
+describe("DOM tests", () => {
+    test("Expects content to change", () => {
+        buttonClick();
+        expect(document.getElementById("par")
+            .innerHTML).toEqual("You Clicked");
+    });
+});
