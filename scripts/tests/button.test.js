2c53997c2138279e0822db830c89e170f194a8cd
ireneuszcierpisz <>
2023-12-10 20:41:40 +0000

    Testing the DOM. Mocks a complete HTML page.

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -4,8 +4,28 @@
 
 const buttonClick = require("../button");
 
+/*
+first aproach is to add  chunks of HTML to Jest's mock DOM for testing
+*/
+// beforeAll(() => {
+//     document.body.innerHTML = "<p id='par'></p>";
+// });
+
+/*
+second aproach is to load the entire HTML page and attach it to the mock DOM. 
+*/
 beforeAll(() => {
-    document.body.innerHTML = "<p id='par'></p>";
+    // add the Node fs(file system) module built into Node,
+    //    that allows us to open, read and write files
+    let fs = require("fs"); //don't need to install fs with npm, because it's part of Node standard library
+
+    //read the index.html file using the utf - 8 character set
+    let fileContents = fs.readFileSync("index.html", "utf-8");  // and store the contents in the fileContents variable
+    
+    //attach the content to the  DOM
+    document.open();
+    document.write(fileContents);  // Loading the entire HTML page into Jest so that we can run tests against it
+    document.close();
 });
 
 // create test
@@ -15,4 +35,9 @@ describe("DOM tests", () => {
         expect(document.getElementById("par")
             .innerHTML).toEqual("You Clicked");
     });
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
+    });
 });
+
+// to run a jest test use a terminal, type command 'npm test' and press Enter
